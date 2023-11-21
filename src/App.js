f934e9c0bb2d7919549ee78d651e4ba4d4c04220
Santiago Bejarano <santiagox2060@gmail.com>
2023-11-20 19:03:35 -0500

    Commit inicial: estructura base del proyecto React con Create React App

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css'; // Importamos Bootstrap para estilos
+
+// Aquí importaremos nuestros componentes personalizados cuando estén listos
+// import WeatherCard from './components/WeatherCard';
+// import SearchBar from './components/SearchBar';
 
 function App() {
+  // Aquí irán los estados y lógica para manejar la data de la API y la búsqueda
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        {/* Acá iría nuestro SearchBar componente */}
+        {/* <SearchBar /> */}
       </header>
+      <main>
+        {/* Acá irían los WeatherCard componentes, posiblemente en un contenedor de grilla */}
+        {/* <WeatherCard /> */}
+      </main>
     </div>
   );
 }
